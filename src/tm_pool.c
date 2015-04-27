bfab5a9c088e02c6c0e5b206886a9740df0d1e53
Garrett Berg <garrett@cloudformdesign.com>
2015-04-27 13:28:14 -0600

    pool: add basic free functionality with alloc

--- src/tm_pool.c
+++ src/tm_pool.c
@@ -1,46 +1,37 @@
-#include "tinymem_pool.h"
+#include "tm_pool.h"
 
 
 /*---------------------------------------------------------------------------*/
 /*      Local Declarations                                                   */
-tm_index_t Pool_find_index(Pool *pool);
-uint8_t freed_bin(tm_size_t size);
-inline void Pool_freed_remove(Pool *pool, tm_index_t index);
-tm_index_t Pool_freed_get(Pool *pool, tm_size_t size);
-
+tm_index_t      Pool_find_index(Pool *pool);
+uint8_t         freed_bin(const tm_blocks_t size);
+inline void     Pool_freed_remove(Pool *pool, const tm_index_t index);
+inline void     Pool_freed_insert(Pool *pool, const tm_index_t index);
+tm_index_t      Pool_freed_get(Pool *pool, const tm_blocks_t size);
 #define Pool_free_p(pool, index)  ((free_block *)Pool_void_p(pool, index))
-#define Pool_freed_insert(pool, index, bin) do{                                     \
-    *Pool_free_p(pool, index) = (free_block){.next=(pool)->freed[bin], .prev=0};    \
-    Pool_free_p(pool, (pool)->freed[bin])->prev = index;                           \
-    }while(0)
-
 
 
 /*---------------------------------------------------------------------------*/
-void Pool_init(Pool *pool){
-    *pool = Pool_declare();
-}
-
-
-/*---------------------------------------------------------------------------*/
-void *Pool_void_p(Pool *pool, tm_index_t index){
+void *          Pool_void_p(const Pool *pool, const tm_index_t index){
     // Note: index 0 has location == heap (it is where Pool_heap is stored)
     if(Pool_loc(pool, index) >= Pool_heap(pool)) return NULL;
     return pool->pool + Pool_loc(pool, index);
 }
 
 /*---------------------------------------------------------------------------*/
-tm_index_t Pool_alloc(Pool *pool, tm_size_t size){
+tm_index_t      Pool_alloc(Pool *pool, tm_size_t size){
     tm_index_t index;
-    size += size % TM_ALIGN_BYTES;
-    if(Pool_available(pool) < size) return 0;
+    size = TM_ALIGN_BLOCKS(size);
+    if(Pool_available_blocks(pool) < size) return 0;
 
     index = Pool_freed_get(pool, size);
     if(index){
-        pool->filled_bytes += size;
-        pool->freed_bytes -= size;
+        pool->filled_blocks += size;
+        pool->freed_blocks -= size;
         pool->ptrs_filled++;
         pool->ptrs_freed--;
+        Pool_filled_set(pool, index);
+        assert(Pool_points_bool(pool, index));
         return index;
     }
     if(Pool_heap_left(pool) < size){
@@ -56,30 +47,34 @@ tm_index_t Pool_alloc(Pool *pool, tm_size_t size){
     Pool_points_set(pool, index);
     pool->pointers[index] = (poolptr) {.loc = Pool_heap(pool), .next = 0};
     Pool_heap(pool) += size;
-    pool->filled_bytes += size;
+    pool->filled_blocks += size;
     pool->ptrs_filled++;
+    if(pool->last_index) pool->pointers[pool->last_index].next = index;
+    pool->last_index = index;
     return index;
 }
 
 /*---------------------------------------------------------------------------*/
-void Pool_free(Pool *pool, tm_index_t index){
+void            Pool_free(Pool *pool, const tm_index_t index){
     uint8_t bin;
     if(Pool_loc(pool, index) >= Pool_heap(pool)) return;
     if(index >= TM_MAX_POOL_PTRS || !Pool_filled_bool(pool, index)) return;
     Pool_filled_clear(pool, index);
-    pool->filled_bytes -= Pool_sizeof(pool, index);
+    pool->filled_blocks -= Pool_blocks(pool, index);
+    pool->freed_blocks += Pool_blocks(pool, index);
     pool->ptrs_filled--;
     pool->ptrs_freed++;
 
     // Note: index 0 == filled
+    tm_debug("Freeing index=%u, next=%u, size=%u", index, Pool_next(pool, index), Pool_sizeof(pool, index));
     if(!Pool_filled_bool(pool, Pool_next(pool, index))){
+        tm_debug("Next is free");
         // The next index is also free, remove from free bin and join
         Pool_freed_remove(pool, Pool_next(pool, index));
         pool->ptrs_freed--;
         Pool_next(pool, index) = Pool_next(pool, Pool_next(pool, index));
     }
-    bin = freed_bin(Pool_sizeof(pool, index));    // speeds up macro
-    Pool_freed_insert(pool, index, bin);
+    Pool_freed_insert(pool, index);
 }
 
 
@@ -87,7 +82,7 @@ void Pool_free(Pool *pool, tm_index_t index){
 /*      Local Functions                                                      */
 
 /*---------------------------------------------------------------------------*/
-tm_index_t Pool_find_index(Pool *pool){
+tm_index_t      Pool_find_index(Pool *pool){
     unsigned int bit;
     if(!Pool_pointers_left(pool)) return 0;
     while(1){
@@ -111,45 +106,54 @@ tm_index_t Pool_find_index(Pool *pool){
 }
 
 /*---------------------------------------------------------------------------*/
-/*      get the freed bin for size                                           */
-uint8_t freed_bin(tm_size_t size){
-    assert(size % TM_ALIGN_BYTES == 0);
-    switch(size){
-        case TM_ALIGN_BYTES:                    return 0;
-        case TM_ALIGN_BYTES * 2:                return 1;
-        case TM_ALIGN_BYTES * 3:                return 2;
+/*      get the freed bin for blocks                                         */
+uint8_t         freed_bin(const tm_blocks_t blocks){
+    switch(blocks){
+        case 1:                     return 0;
+        case 2:                     return 1;
+        case 3:                     return 2;
     }
-    if(size < TM_ALIGN_BYTES * 64){
-        if(size < TM_ALIGN_BYTES * 8)           return 3;
-        else if(size < TM_ALIGN_BYTES * 16)     return 4;
-        else if(size < TM_ALIGN_BYTES * 32)     return 5;
-        else                                    return 6;
+    if(blocks < 64){
+        if(blocks < 8)              return 3;
+        else if(blocks < 16)        return 4;
+        else if(blocks < 32)        return 5;
+        else                        return 6;
     }
-    else if(size < TM_ALIGN_BYTES * 1024){
-        if(size < TM_ALIGN_BYTES * 128)         return 7;
-        else if(size < TM_ALIGN_BYTES * 256)    return 8;
-        else if(size < TM_ALIGN_BYTES * 512)    return 9;
-        else                                    return 10;
+    else if(blocks < 1024){
+        if(blocks < 128)            return 7;
+        else if(blocks < 256)       return 8;
+        else if(blocks < 512)       return 9;
+        else                        return 10;
     }
-    else                                        return 11;
+    else                            return 11;
 }
 
 
-inline void Pool_freed_remove(Pool *pool, tm_index_t index){
+inline void     Pool_freed_remove(Pool *pool, const tm_index_t index){
     free_block *free = Pool_free_p(pool, index);
+    assert(free);
     if(free->prev){
         Pool_free_p(pool, free->prev)->next = free->next;
     } else{ // free is first element in the array
-        pool->freed[freed_bin(Pool_sizeof(pool, index))] = free->next;
+        pool->freed[freed_bin(Pool_blocks(pool, index))] = free->next;
     }
     if(free->next) Pool_free_p(pool, free->next)->prev = free->prev;
-    Pool_points_clear(pool, index);
 }
 
 
-tm_index_t Pool_freed_get(Pool *pool, tm_size_t size){
-    assert(size % TM_ALIGN_BYTES == 0);
-    uint8_t bin = freed_bin(size);
+inline void     Pool_freed_insert(Pool *pool, const tm_index_t index){
+    uint8_t bin = freed_bin(Pool_blocks(pool, index));
+    *Pool_free_p(pool, index) = (free_block){.next=pool->freed[bin], .prev=0};
+    if(pool->freed[bin]){
+        Pool_free_p(pool, pool->freed[bin])->prev = index;
+    }
+    pool->freed[bin] = index;
+}
+
+
+tm_index_t      Pool_freed_get(Pool *pool, const tm_blocks_t blocks){
+    uint8_t bin;
+    bin = freed_bin(blocks);
     tm_index_t index;
     for(; bin<TM_FREED_BINS; bin++){
         if(pool->freed[bin]){
@@ -163,8 +167,9 @@ tm_index_t Pool_freed_get(Pool *pool, tm_size_t size){
 
 /*---------------------------------------------------------------------------*/
 /*      For Debug and Test                                                   */
-tm_index_t Pool_freed_count_bin(Pool *pool, uint8_t bin, tm_size_t *size){
+tm_index_t      Pool_freed_count_bin(Pool *pool, uint8_t bin, tm_size_t *size, bool pnt){
     // Get the number and the size of the items in bin
+    tm_index_t prev_index;
     tm_index_t index = pool->freed[bin];
     tm_index_t count = 0;
     *size = 0;
@@ -173,39 +178,66 @@ tm_index_t Pool_freed_count_bin(Pool *pool, uint8_t bin, tm_size_t *size){
     assert(free);
     assert(free->prev == 0);
     while(1){
+        if(pnt) printf("        prev=%u, ind=%u, next=%u\n", free->prev, index, free->next);
         *size += Pool_sizeof(pool, index);
         count++;
+        prev_index = index;
         index = free->next;
-        if(!index) return count;
+        if(!free->next) return count;
         free = Pool_free_p(pool, index);
         assert(free);
-        assert(free->prev == index);
+        assert(free->prev == prev_index);
     }
 }
 
-tm_index_t Pool_freed_count(Pool *pool, tm_size_t *size){
+tm_index_t      Pool_freed_count_print(Pool *pool, tm_size_t *size, bool pnt){
     uint8_t bin;
     tm_size_t size_get;
     tm_index_t count = 0;
     *size = 0;
     for(bin=0; bin<TM_FREED_BINS; bin++){
-        count += Pool_freed_count_bin(pool, bin, &size_get);
+        count += Pool_freed_count_bin(pool, bin, &size_get, pnt);
         *size += size_get;
     }
+    assert(count==pool->ptrs_freed);
+    assert(*size==pool->freed_blocks * TM_ALIGN_BYTES);
     return count;
 }
 
+tm_index_t Pool_freed_count(Pool *pool, tm_size_t *size){
+    return Pool_freed_count_print(pool, size, false);
+}
 
-tm_index_t Pool_freed_print(Pool *pool){
+bool            Pool_freed_isvalid(Pool *pool){
+    tm_size_t size;
+    tm_index_t count = Pool_freed_count(pool, &size);
+    tm_debug("size before=%u", size);
+    size = TM_ALIGN_BLOCKS(size);
+    if(!((count==pool->ptrs_freed) && (size==pool->freed_blocks))){
+        tm_debug("freed: %u==%u", count, pool->ptrs_freed);
+        tm_debug("size:  %u==%u", size, pool->freed_blocks);
+        return false;
+    }
+    return true;
+}
+
+
+tm_index_t      Pool_freed_full_print(Pool *pool, bool full){
     uint8_t bin;
     tm_size_t size = 0, size_get;
     tm_index_t count = 0, count_get;
     printf("## Freed Bins:\n");
     for(bin=0; bin<TM_FREED_BINS; bin++){
-        count_get = Pool_freed_count_bin(pool, bin, &size_get);
-        printf("    bin %u: size=%u, count=%u\n", bin, size_get, count_get);
+        count_get = Pool_freed_count_bin(pool, bin, &size_get, full);
+        if(count_get) printf("    bin %4u: size=%-8u count=%-8u\n", bin, size_get, count_get);
         count += count_get;
         size += size_get;
     }
     printf("TOTAL: size=%u, count=%u\n", size, count);
 }
+
+
+tm_index_t      Pool_freed_print(Pool *pool){
+    Pool_freed_full_print(pool, false);
+}
+
--- src/tm_pool.h
+++ src/tm_pool.h
@@ -1,6 +1,6 @@
 #ifndef __tinymem_pool_h
 #define __tinymem_pool_h
-#include "tinymem_types.h"
+#include "tm_types.h"
 
 
 //#if (TM_POOL_SIZE % sizeof(free_block))
@@ -11,6 +11,8 @@
 //#error "Invalid pool ptrs size, must be divisible by int"
 //#endif
 
+#define CEILING(x, y)           (((x) % (y)) ? (x)/(y) + 1 : (x)/(y))
+
 #define TM_POOL_BLOCKS          (TM_POOL_SIZE / sizeof(free_block))
 #define TM_MAX_BIT_INDEXES      (TM_MAX_POOL_PTRS / (8 * sizeof(int)))
 #define MAXUINT                 ((unsigned int) 0xFFFFFFFFFFFFFFFF)
@@ -18,6 +20,10 @@
 
 #define TM_FREED_BINS           (12)
 #define TM_ALIGN_BYTES          sizeof(free_block)
+#define TM_ALIGN(size)          (((size) % TM_ALIGN_BYTES) ? \
+    ((size) + TM_ALIGN_BYTES - ((size) % TM_ALIGN_BYTES)): (size))
+#define TM_ALIGN_BLOCKS(size)   CEILING(size, TM_ALIGN_BYTES)
+
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -25,10 +31,9 @@
  */
 // TODO: packed!
 typedef struct {
-    tm_size_t loc;
+    tm_blocks_t loc;
     tm_index_t next;
 } poolptr;
-#define NULL_poolptr            ((poolptr){.size=0, .ptr=0})
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -53,11 +58,12 @@ typedef struct {
     unsigned int    points[TM_MAX_BIT_INDEXES];     //!< bit array of used pointers (both used and freed)
     poolptr         pointers[TM_MAX_POOL_PTRS];     //!< This is the index lookup location
     tm_index_t      freed[TM_FREED_BINS];           //!< binned storage of all freed indexes
-    tm_size_t       filled_bytes;                   //!< total amount of data allocated
-    tm_size_t       freed_bytes;                    //!< total amount of data freed
-    tm_index_t      ptrs_filled;                     //!< total amount of pointers allocated
-    tm_index_t      ptrs_freed;                      //!< total amount of pointers freed
+    tm_blocks_t       filled_blocks;                //!< total amount of data allocated
+    tm_blocks_t       freed_blocks;                 //!< total amount of data freed
+    tm_index_t      ptrs_filled;                    //!< total amount of pointers allocated
+    tm_index_t      ptrs_freed;                     //!< total amount of pointers freed
     tm_index_t      find_index;                     //!< speed up find index
+    tm_index_t      last_index;                     //!< required to allocate off heap
     uint8_t         status;                         //!< status byte. Access with Pool_status macros
     uint8_t         find_index_bit;                 //!< speed up find index
 } Pool;
@@ -68,16 +74,17 @@ typedef struct {
  * \brief           Internal use only. Declares Pool with as many initial
  *                  values as possible
  */
-#define Pool_declare()  ((Pool) {                               \
-    .filled = {0},                                              \
+#define Pool_init()  ((Pool) {                                  \
+    .filled = {1},                      /*NULL is taken*/       \
     .points = {1},                      /*NULL is taken*/       \
     .pointers = {{0, 0}},               /*heap = 0*/            \
     .freed = {0},                                               \
-    .filled_bytes = {0},                                        \
-    .freed_bytes = {0},                                         \
-    .ptrs_filled = 1,                    /*NULL is "filled"*/    \
-    .ptrs_freed = 0,                                             \
+    .filled_blocks = {0},                                       \
+    .freed_blocks = {0},                                        \
+    .ptrs_filled = 1,                    /*NULL is "filled"*/   \
+    .ptrs_freed = 0,                                            \
     .find_index = 0,                                            \
+    .last_index = 0,                                            \
     .status = 0,                                                \
     .find_index_bit = 1,                /*index 0 is invalid*/  \
 })
@@ -88,13 +95,14 @@ typedef struct {
  *                  Pool_filled* does operations on Pool's `filled` array
  *                  Pool_points* does operations on Pool's `points` array
  */
-#define Pool_available(p)            (TM_POOL_SIZE - (p)->filled_bytes)
+#define Pool_available_blocks(p)     (TM_POOL_BLOCKS - (p)->filled_blocks)
+#define Pool_available(p)            (Pool_available_blocks(p) * TM_ALIGN_BYTES)
 #define Pool_pointers_left(p)        (TM_MAX_POOL_PTRS - ((p)->ptrs_filled + (p)->ptrs_filled))
 #define Pool_heap_left(p)            (TM_POOL_SIZE - Pool_heap(p))
-#define Pool_loc(p, index)              ((p)->pointers[index].loc)
+#define Pool_loc(p, index)           ((p)->pointers[index].loc)
 #define Pool_heap(p)                 ((p)->pointers[0].loc)
 #define Pool_next(p, index)          ((p)->pointers[index].next)
-#define Pool_loc_void_p(p, loc)           ((void*)(p)->pool + (loc))
+#define Pool_loc_void_p(p, loc)      ((void*)(p)->pool + (loc))
 
 #define BITARRAY_INDEX(index)        (index / 8)
 #define BITARRAY_BIT(index)          (1 << (index % 8))
@@ -121,19 +129,15 @@ typedef struct {
 #define Pool_status_set(p, name)     ((p)->status |= (name))
 #define Pool_status_clear(p, name)   ((p)->status &= ~(name))
 
-/*---------------------------------------------------------------------------*/
-/**
- * \brief           initialize (or reset) a pool
- */
-void            Pool_init(Pool *pool);
 
 /*---------------------------------------------------------------------------*/
 /**
  * \brief           Get the sizeof data at index in bytes
  * \return tm_size_t  the sizeof the data pointed to by index
  */
-#define Pool_sizeof(p, index)        (Pool_loc(p, (p)->pointers[index].next) - Pool_loc(p, index))
-
+#define Pool_sizeof(p, index)        (Pool_blocks(p, index) * TM_ALIGN_BYTES)
+#define Pool_blocks(p, index)        (Pool_loc(p, (p)->pointers[index].next) - \
+                                      Pool_loc(p, index))
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -143,7 +147,7 @@ void            Pool_init(Pool *pool);
  * \return          tm_index_t corresponding to memory location
  *                  On error or if not enough memory, return value == 0
  */
-tm_index_t        Pool_alloc(Pool *pool, tm_size_t size);
+tm_index_t          Pool_alloc(Pool *pool, tm_size_t size);
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -159,7 +163,7 @@ tm_index_t        Pool_alloc(Pool *pool, tm_size_t size);
  *                  If return value == 0, then no change has been done
  *                  (or index has been freed if size=0)
  */
-tm_index_t        Pool_realloc(Pool *pool, tm_index_t index, tm_size_t size);
+tm_index_t          Pool_realloc(Pool *pool, tm_index_t index, tm_size_t size);
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -167,7 +171,7 @@ tm_index_t        Pool_realloc(Pool *pool, tm_index_t index, tm_size_t size);
  * \param pool      pointer to Pool struct
  * \param index     tm_index_t to free
  */
-void            Pool_free(Pool *pool, tm_index_t index);
+void                Pool_free(Pool *pool, const tm_index_t index);
 
 
 /*---------------------------------------------------------------------------*/
@@ -177,7 +181,7 @@ void            Pool_free(Pool *pool, tm_index_t index);
  * \param index     tm_index_t to get pointer to
  * \return          void* pointer to actual data
  */
-inline void*    Pool_void_p(Pool *pool, tm_index_t index);
+inline void*        Pool_void_p(const Pool *pool, const tm_index_t index);
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -193,8 +197,8 @@ inline void*    Pool_void_p(Pool *pool, tm_index_t index);
 
 /*---------------------------------------------------------------------------*/
 /*      For Debug and Test                                                   */
-tm_index_t Pool_freed_count(Pool *pool, tm_size_t *size);
-tm_index_t Pool_freed_print(Pool *pool);
+tm_index_t          Pool_freed_count(Pool *pool, tm_size_t *size);
+tm_index_t          Pool_freed_print(Pool *pool);
 
 #endif
 /** @} */
